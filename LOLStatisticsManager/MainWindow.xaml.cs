853e74bbba1895ff5f38c82162d2ca1ca4c3896c
tarantula251 <justyna.zofia.gasior@gmail.com>
2019-11-05 22:06:49 +0100

    Finish styling for main window, add events for collecting search data

--- LOLStatisticsManager/MainWindow.xaml.cs
+++ LOLStatisticsManager/MainWindow.xaml.cs
@@ -1,29 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using System.Collections.ObjectModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LOLStatisticsManager
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Location germany;
+        public ObservableCollection<string> Localizations { get; set; }
+        public string selectedCountry { get; set; }
+        public string searchTerm { get; set; }
         public MainWindow()
         {
+            Localizations = new ObservableCollection<string>() { "NA", "EUW", "EUNE", "BR", "JP", "KR", "LAN", "LAS", "OCE", "RU", "TR" };
+
             InitializeComponent();
+            DataContext = this;
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = (ListBox)sender;
+            selectedCountry = (string)item.SelectedItem;            
+        }
+
+        private void searchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var item = (TextBox)sender;
+            searchTerm = (string)item.Text;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO use these fields for search purposes, open result window
+            //MessageBox.Show("Your country " + selectedCountry + " search term " + searchTerm);
         }
     }
 }
