96ab79d1fd003230a44032d18afa787337fc7d6d
Rhathin <Rhathin@gmail.com>
2019-11-11 22:13:47 +0100

    Add controller class for Riot API
    This class will give interface for extracting LoL data from Riot API.

---
+++ LOLStatisticsManager/Controller/RiotAPIController.cs
@@ -0,0 +1,21 @@
+﻿using LOLStatisticsManager.Model;
+using LOLStatisticsManager.Model.API;
+
+namespace LOLStatisticsManager.Controller
+{
+    class RiotAPIController
+    {
+        public string Region { get; set; }
+
+        public RiotAPIController(string region)
+        {
+            Region = region;
+        }
+
+        public SummonerDTO GetSummonerByName(string summonerName)
+        {
+            Summoner_V4 summoner_V4 = new Summoner_V4(Region);
+            return summoner_V4.GetSummonerByName(summonerName);
+        }
+    }
+}
--- LOLStatisticsManager/ResultPage.xaml.cs
+++ LOLStatisticsManager/ResultPage.xaml.cs
@@ -4,6 +4,9 @@ using System.Net;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
+using LOLStatisticsManager.Controller;
+using LOLStatisticsManager.Model;
+
 namespace LOLStatisticsManager
 {
     /// <summary>
@@ -46,7 +49,24 @@ namespace LOLStatisticsManager
             image.StreamSource = memoryStream;
             image.EndInit();
 
-            imageHolder.Source = image;            
+            imageHolder.Source = image;
+
+            /*
+             * Example code for controller class usage
+             */
+
+            //Create new controller for given region
+            RiotAPIController controller = new RiotAPIController("eun1"); //EUNE = eun1 https://developer.riotgames.com/docs/lol#_routing-values
+
+            //Get summoner data for given name. Summoner data is stored in SummonerDTO class https://developer.riotgames.com/apis#summoner-v4/GET_getBySummonerName
+            SummonerDTO summoner = controller.GetSummonerByName("Rattin"); //Name will be get from SearchPage
+
+            //GetSummonerByName method will return null if anything went wrong
+            if (summoner != null) summonerDataTextBlock.Text = "Summoner: " + summoner.Name + "\nLevel: " + summoner.SummonerLevel;
+            
+            /*
+             * End of example
+             */
         }
     }
 }
